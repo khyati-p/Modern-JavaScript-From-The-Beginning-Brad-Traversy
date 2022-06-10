3bfbc5adbe62b63ed41ce7a9415acad66004ec66
Khyati Polukonda <khyati_polukonda@epam.com>
2022-06-10 16:31:51 +0530

    day-1

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+//alert in window
+alert("khyatiiii")
+*/
+
+//log to console
+console.log("logginubjhvjh");
+console.log(123);
+console.log(true);
+var a = 977979;
+console.log(['v',8,9,false,a],a);//array
+console.log({a:1,2:'b', 3:'p',n:true,j:a})//object
+console.table({a:1,2:'b', 3:'p',n:true,j:a});
